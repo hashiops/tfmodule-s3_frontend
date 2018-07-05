8dd4e0af25da199e7c65735620ab73cc30279246
aliaksei maiseyeu <>
2018-07-05 11:34:02 +0300

    add aditional output

--- output.tf
+++ output.tf
@@ -1,3 +1,11 @@
 output "cf_domain_name" {
   value = "${module.cloudfront-s3-cdn.cf_domain_name}"
 }
+
+output "s3_bucket_id" {
+  value = "${module.cloudfront-s3-cdn.s3_bucket}"
+}
+
+output "s3_bucket_domain_name" {
+  value = "${module.cloudfront-s3-cdn.bucket_domain_name}"
+}
