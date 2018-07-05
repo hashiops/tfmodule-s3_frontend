841e701f32fa29ec61eede24f3344e935f3107e8
aliaksei maiseyeu <>
2018-07-05 16:47:17 +0300

    add attributes

--- input.tf
+++ input.tf
@@ -1,9 +1,20 @@
 variable "name" {}
 variable "stage" {}
 variable "namespace" {}
+
+variable "attributes" {
+  type    = "list"
+  default = []
+}
+
 variable "cloudfront_cert_arn" {}
-variable "aliases" { type = "list" }
+
+variable "aliases" {
+  type = "list"
+}
+
 variable "zone_id" {}
+
 variable "default_root_object" {
   default = "index.html"
 }
--- main.tf
+++ main.tf
@@ -1,15 +1,15 @@
-
-
 module "cloudfront-s3-cdn" {
-  source  = "github.com/cloudposse/terraform-aws-cloudfront-s3-cdn"
+  source = "github.com/cloudposse/terraform-aws-cloudfront-s3-cdn"
 
-  name = "${var.name}"
-  stage = "${var.stage}"
-  namespace = "${var.namespace}"
+  name                = "${var.name}"
+  stage               = "${var.stage}"
+  namespace           = "${var.namespace}"
   acm_certificate_arn = "${var.cloudfront_cert_arn}"
 
-  aliases = "${var.aliases}"
+  aliases        = "${var.aliases}"
   parent_zone_id = "${var.zone_id}"
+
+  attributes = "${var.attributes}"
 
   default_root_object = "${var.default_root_object}"
 
